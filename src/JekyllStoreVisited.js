2d34f9e785aa097643db90c46b81f7d2474d2db6
Max White <mushishi78@gmail.com>
2015-04-28 20:28:11 +0100

    Or equals for visit action

--- src/JekyllStoreVisited.js
+++ src/JekyllStoreVisited.js
@@ -1,6 +1,6 @@
 var Reflux = require('reflux');
 var JSE = require('jekyll-store-engine');
 
-JSE.Actions.visit = Reflux.createAction();
+JSE.Actions.visit = JSE.Actions.visit || Reflux.createAction();
 JSE.Actions.setVisitedLimit = Reflux.createAction();
 JSE.Stores.Visited = require('./VisitedStore');
